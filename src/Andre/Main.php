6a79d9f37d0b2e6ef6d309cfb8d614e8c536c804
Andre <andrethegamer911@gmail.com>
2016-05-03 19:05:09 -0400

    Main file finished

---
+++ src/Andre/Main.php
@@ -0,0 +1,30 @@
+<?php
+namespace Andre;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\Position;
+use pocketmine\utils\TextFormat as color;
+
+   class Main extends PluginBase implements Listener{
+   public function onEnable(){
+$this->getServer()->getLogger()->info("Position has been loaded");
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+}
+public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        switch ($command){
+            case 'xyz':
+			$x = $p->getX();
+			$y = $p->gety();
+                        $z = $p->getZ();
+				$sender->sendMessage("You are standing at: $x , $y , $z");
+		}
+}
+public function Disable(){
+$this->getServer()->getLogger()->info("Position unloaded");
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+}
+}
